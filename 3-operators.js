0631f91a65d7b340f4a5b062f35035ae661f8af5
Dalton mwadime <emojimwadime@gmail.com>
2026-04-26 22:57:38 +0300

    Update 3-operators.js

---
+++ 3-operators.js
@@ -0,0 +1,174 @@
+// An operator is a special symbol that operates on one or more operands.
+// In JavaScript, there are several types of operators, including:
+// 1. Arithmetic operators: +, -, *, /, %, ++, --
+// 2. Assignment operators: =, +=, -=, *=, /=, %=
+// 3. Comparison operators: ==, ===, !=, !==, >, <, >=, <=
+// 4. Logical operators: &&, ||, !
+// 5. Bitwise operators: &, |, ^, ~, <<, >>, >>>
+// 6. Ternary operator:? :
+// 7. Type operators: typeof, instanceof, in
+// 8. Spread operator: ...
+// 9. Rest operator: ...
+// 10. Nullish coalescing operator: ??
+// 11. Optional chaining operator: ?.
+// 12. Exponentiation operator: **
+// 13. Comma operator:,
+// 14. Unary operators: +, -, ++, --, typeof, void, delete
+// 15. Relational operators: in, instanceof
+// 16. Logical assignment operators: &&=, ||=, ??=
+// 17. Bitwise assignment operators: &=, |=, ^=, <<=, >>=, >>>=
+// 18. Destructuring assignment operators: =, ...
+// 19. Spread syntax: ...
+// 20. Rest syntax: ...
+// 21. Optional chaining: ?.
+// 22. Nullish coalescing: ??
+// 23. Exponentiation: **
+// 24. Comma operator: ,
+// 25. Unary operators: +, -, ++, --, typeof, void, delete
+// 26. Relational operators: in, instanceof
+// 27. Logical assignment operators: &&=, ||=, ??=
+// 28. Bitwise assignment operators: &=, |=, ^=, <<=, >>=, >>>=
+// 29. Destructuring assignment operators: =, ...
+// 30. Spread syntax: ...
+// 31. Rest syntax: ...
+// 32. Optional chaining: ?.
+// 33. Nullish coalescing: ??
+// 34. Exponentiation: **
+// 35. Comma operator:,
+// 36. Unary operators: +, -, ++, --, typeof, void, delete
+// 37. Relational operators: in, instanceof
+// 38. Logical assignment operators: &&=, ||=, ??=
+// 39. Bitwise assignment operators: &=, |=, ^=, <<=, >>=, >>>=
+// 40. Destructuring assignment operators: =, ...
+// 41. Spread syntax: ...
+// 42. Rest syntax: ...
+// 43. Optional chaining: ?.
+// 44. Nullish coalescing: ??   
+// 45. Exponentiation: **
+// 46. Comma operator:,
+// 47. Unary operators: +, -, ++, --, typeof, void, delete
+// 48. Relational operators: in, instanceof
+// 49. Logical assignment operators: &&=, ||=, ??=
+// 50. Bitwise assignment operators: &=, |=, ^=, <<=, >>=, >>>=
+// 51. Destructuring assignment operators: =, ...
+// 52. Spread syntax: ...
+// 53. Rest syntax: ...
+// 54. Optional chaining: ?.
+// 55. Nullish coalescing: ??
+// 56. Exponentiation: **
+// 57. Comma operator:,
+// 58. Unary operators: +, -, ++, --, typeof, void, delete
+// 59. Relational operators: in, instanceof
+// 60. Logical assignment operators: &&=, ||=, ??=
+// 61. Bitwise assignment operators: &=, |=, ^=, <<=, >>=, >>>=
+// 62. Destructuring assignment operators: =, ...
+// 63. Spread syntax: ...
+// 64. Rest syntax: ...
+// 65. Optional chaining: ?.
+// 66. Nullish coalescing: ??
+// 67. Exponentiation: **
+// 68. Comma operator:,    
+// 69. Unary operators: +, -, ++, --, typeof, void, delete
+// 70. Relational operators: in, instanceof
+// 71. Logical assignment operators: &&=, ||=, ??=
+// 72. Bitwise assignment operators: &=, |=, ^=, <<=, >>=, >>>=
+// 73. Destructuring assignment operators: =, ...
+// 74. Spread syntax: ...
+// 75. Rest syntax: ...
+// 76. Optional chaining: ?.
+// 77. Nullish coalescing: ??
+// 78. Exponentiation: **
+// 79. Comma operator:,    
+// 80. Unary operators: +, -, ++, --, typeof, void, delete
+// 81. Relational operators: in, instanceof
+// 82. Logical assignment operators: &&=, ||=, ??=
+// 83. Bitwise assignment operators: &=, |=, ^=, <<=, >>=, >>>=
+// 84. Destructuring assignment operators: =, ...
+// 85. Spread syntax: ...
+// 86. Rest syntax: ...
+// 87. Optional chaining: ?.
+// 88. Nullish coalescing: ??
+// 89. Exponentiation: **
+// 90. Comma operator:,    
+// 91. Unary operators: +, -, ++, --, typeof, void, delete
+// 92. Relational operators: in, instanceof
+// 93. Logical assignment operators: &&=, ||=, ??=
+// 94. Bitwise assignment operators: &=, |=, ^=, <<=, >>=, >>>=
+// 95. Destructuring assignment operators: =, ...
+// 96. Spread syntax: ...
+// 97. Rest syntax: ...
+// 98. Optional chaining: ?.
+// 99. Nullish coalescing: ??
+// 100. Exponentiation: **
+// In JavaScript, we can also use the typeof operator to check the data type of a variable.
+console.log(typeof name);   
+console.log(typeof age);    
+console.log(typeof isStudent);
+console.log(typeof emptyValue);
+console.log(typeof uninitializedVariable);  
+console.log(typeof id);
+console.log(typeof bigIntValue);
+console.log(typeof person); 
+// In JavaScript, we can also use the instanceof operator to check if an object is an instance of a particular class or constructor function.
+console.log(name instanceof String);
+console.log(age instanceof Number); 
+console.log(isStudent instanceof Boolean);  
+console.log(emptyValue instanceof Null);
+console.log(uninitializedVariable instanceof Undefined);
+console.log(id instanceof Symbol);
+console.log(bigIntValue instanceof BigInt);
+console.log(person instanceof Object);
+// In JavaScript, we can also use the constructor property to check the constructor function of an object.
+console.log(name.constructor);
+console.log(age.constructor);
+console.log(isStudent.constructor);
+console.log(emptyValue.constructor);
+console.log(uninitializedVariable.constructor);
+console.log(id.constructor);
+console.log(bigIntValue.constructor);
+console.log(person.constructor);
+// In JavaScript, we can also use the Object.prototype.toString() method to get the internal [[Class]] property of an object, which can be used to determine the type of an object.
+console.log(Object.prototype.toString.call(name));
+console.log(Object.prototype.toString.call(age));
+console.log(Object.prototype.toString.call(isStudent));
+console.log(Object.prototype.toString.call(emptyValue));
+console.log(Object.prototype.toString.call(uninitializedVariable));
+console.log(Object.prototype.toString.call(id));
+console.log(Object.prototype.toString.call(bigIntValue));
+console.log(Object.prototype.toString.call(person));
+// In JavaScript, we can also use the typeof operator to check the data type of a variable.
+console.log(typeof name);   
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof emptyValue);
+console.log(typeof uninitializedVariable);  
+console.log(typeof id);
+console.log(typeof bigIntValue);
+console.log(typeof person);
+// In JavaScript, we can also use the instanceof operator to check if an object is an instance of a particular class or constructor function.
+console.log(name instanceof String);
+console.log(age instanceof Number); 
+console.log(isStudent instanceof Boolean); 
+console.log(emptyValue instanceof Null);
+console.log(uninitializedVariable instanceof Undefined);
+console.log(id instanceof Symbol);
+console.log(bigIntValue instanceof BigInt);
+console.log(person instanceof Object);
+// In JavaScript, we can also use the constructor property to check the constructor function of an object.
+console.log(name.constructor);
+console.log(age.constructor);
+console.log(isStudent.constructor);
+console.log(emptyValue.constructor);
+console.log(uninitializedVariable.constructor);
+console.log(id.constructor);    
+console.log(bigIntValue.constructor);
+console.log(person.constructor);
+// In JavaScript, we can also use the Object.prototype.toString() method to get the internal [[Class]] property of an object, which can be used to determine the type of an object.
+console.log(Object.prototype.toString.call(name));
+console.log(Object.prototype.toString.call(age));   
+console.log(Object.prototype.toString.call(isStudent));
+console.log(Object.prototype.toString.call(emptyValue));
+console.log(Object.prototype.toString.call(uninitializedVariable));
+console.log(Object.prototype.toString.call(id));
+console.log(Object.prototype.toString.call(bigIntValue));
+console.log(Object.prototype.toString.call(person));
