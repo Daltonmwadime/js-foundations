a4199b4abbd0fbc4e9f2d64a4aa52a4173223c4b
Dalton mwadime <emojimwadime@gmail.com>
2026-05-11 13:03:52 +0300

    Document JavaScript conditional statements
    Added explanations for various conditional statements in JavaScript.

---
+++ 6-cf-conditionals.js
@@ -0,0 +1,32 @@
+// a conditional statement is a programming construct that allows you to execute different blocks of code based on certain conditions. In JavaScript, there are several types of conditional statements, including if statements, else statements, else if statements, and switch statements.
+//The if statement is used to execute a block of code if a specified condition is true. The syntax for an if statement is as follows:
+if (condition) {
+    // code to be executed if the condition is true
+}
+//The else statement is used to execute a block of code if the condition in the if statement is false. The syntax for an else statement is as follows:
+if (condition) {
+    // code to be executed if the condition is true
+} else {
+    // code to be executed if the condition is false
+}
+//The else if statement is used to specify a new condition to test if the first condition is false. The syntax for an else if statement is as follows:
+if (condition1) {
+    // code to be executed if condition1 is true
+} else if (condition2) {
+    // code to be executed if condition2 is true
+} else {
+    // code to be executed if both condition1 and condition2 are false
+}
+//The switch statement is used to perform different actions based on different conditions. The syntax for a switch statement is as follows:
+switch (expression) {
+    case value1:
+        // code to be executed if expression equals value1
+        break;
+    case value2:
+        // code to be executed if expression equals value2
+        break;
+    default:
+        // code to be executed if expression does not match any case
+}
+ //In JavaScript, conditional statements are essential for controlling the flow of a program and making decisions based on user input, data, or other conditions. They allow developers to create dynamic and interactive applications that can respond to different scenarios and user interactions.
+ 
